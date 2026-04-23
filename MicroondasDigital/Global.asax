ad57d894c65d4195dd5df6e1f3187e9ec8da53f4
Vinicius-Toledo <vini_toledo_@hotmail.com>
2026-04-23 11:21:36 -0300

    Adicionar arquivos de projeto.

---
+++ MicroondasDigital/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MicroondasDigital.MvcApplication" Language="C#" %>
---
+++ MicroondasDigital/Models/MicroondasModels.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace MicroondasDigital.Models
+{
+    public class MicroondasModel
+    {
+     
+        private int _tempo;
+        private int _potencia;
+    
+        private const int TempoMaximo = 120;
+        private const int TempoMinimo = 1;
+        private const int PotenciaPadrao = 10;
+
+        public string IniciarAquecimento(int segundos, int? potenciaInput)
+        {
+            if (segundos < TempoMinimo || segundos > TempoMaximo)
+            {
+                return "Erro: O tempo deve ser entre 1 e 120 segundos.";
+            }
+
+            _tempo = segundos;
+            _potencia = potenciaInput ?? PotenciaPadrao;
+
+            StringBuilder progresso = new StringBuilder();
+            for (int i = 0; i < _tempo; i++)
+            {
+                progresso.Append(".");
+            }
+
+            return progresso.ToString() + " Aquecimento concluído";
+        }
+    }
+}
